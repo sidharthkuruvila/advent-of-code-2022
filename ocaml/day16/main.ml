7fdf7c8cb7a0e17a78601a42c1e2b8c8ee4ee7d3
Sidharth Kuruvila <sidharth.kuruvila@gmail.com>
2022-12-24 07:01:24 +0530

    day16: Cache the state

--- ocaml/day16/main.ml
+++ ocaml/day16/main.ml
@@ -35,66 +35,56 @@ let shortest_paths paths rates =
                                           |>  List.concat_map ~f:(fun d -> [(a, (b, d)); (b, (a, d))]))
   |> String.Map.of_alist_multi
 
-type actor_state = {
-    idx: int;
-    rate: int;
-    total_volume: int;
-    minutes_remaining: int;
-    current_room: string;
-  }
-  
-type cave_state = {
+module Actor_state = struct  
+  type t = {
+      rate: int;
+      total_volume: int;
+      minutes_remaining: int;
+      current_room: string;
+    } [@@deriving sexp, compare]
+end
 
-    actors: actor_state list;
+module Cave_state = struct
+type t = {
+    actors: Actor_state.t list;
     closed_valves: String.Set.t;
-    trace: string list;
-  }
+  } [@@deriving sexp, compare]
+end
 
-let add_trace rates state message =
-  let { closed_valves; actors; trace; _} = state in
-  let original_open_valves =  String.Map.filter rates  ~f:(fun r -> r > 0) |> String.Map.keys |> String.Set.of_list in
-  let open_valves = String.Set.diff  original_open_valves closed_valves |> String.Set.to_list |> String.concat ~sep:", " in
-  let actor_traces = List.map actors ~f:(fun {idx; rate; minutes_remaining; current_room; _} ->
-                         Printf.sprintf "Actor %d: Rate: %d, Minutes remaining: %d, Current room: %s" idx rate minutes_remaining current_room)
-                     |> String.concat ~sep:"\n" in
-  let total_volume = List.sum (module Int) actors ~f:(fun {total_volume; _} -> total_volume) in
-  let msg = Printf.sprintf {|==  ==
-Valves %s are open, total %d pressure released.
-%s
-%s|} (open_valves) (total_volume) actor_traces message in
-  {state with trace = msg::trace}
+module Cave_state_map = Map.Make(Cave_state)
+                      
   
-let rec walk_paths_with_distance paths rates state =
-  let { closed_valves; actors; _} = state in
-  let  [@warning "-8"] actor::rest_actors = List.sort actors ~compare:(fun a1 a2 -> Int.compare a2.minutes_remaining a1.minutes_remaining) in
-  let { minutes_remaining; rate; total_volume; current_room; _} = actor in
-  if minutes_remaining = 0 then begin
-    assert (List.for_all actors ~f:(fun { minutes_remaining; _ } -> minutes_remaining = 0));
-    let state = add_trace rates state "Ending early - Done processing" in
-    [ List.sum (module Int) actors ~f:(fun {total_volume; _} -> total_volume), state.trace]
-  end else if String.Set.length closed_valves = 0 then
-    let state = add_trace rates state "All valves opened" in
-    [ (List.sum (module Int) actors ~f:(fun {total_volume; rate; minutes_remaining; _} -> total_volume + rate * minutes_remaining)), state.trace]
+let rec walk_paths_with_distance paths rates state ~state_map =
+  if Cave_state_map.mem state_map state then
+    (Cave_state_map.find_exn state_map state, state_map)
   else
-    String.Map.find_multi paths current_room
-    |> List.filter ~f:(fun (room, _) -> String.Set.mem closed_valves room)
-    |> List.concat_map ~f:(fun (new_current_room, distance) ->
-           let available_distance = if distance < minutes_remaining then distance else minutes_remaining - 1 in
-           let state = add_trace rates state (Printf.sprintf "Moving from %s to %s and opening valve" current_room new_current_room) in
-           let new_rate = rate + String.Map.find_exn rates new_current_room in
-           let total_volume = total_volume + rate * (available_distance + 1) in
-           (*assert (not (String.Set.mem closed_valves current_room)); *)
-           let closed_valves = String.Set.remove closed_valves new_current_room in
-           let actor = {actor with minutes_remaining = minutes_remaining - available_distance - 1; total_volume;  rate = new_rate; current_room = new_current_room  } in
-           let state = {state with closed_valves; actors = actor::rest_actors} in
-           if available_distance < distance then
-             let state = add_trace rates state "No more time" in
-             [List.sum (module Int) state.actors ~f:(fun {total_volume; _} -> total_volume), state.trace]
-           else
-             walk_paths_with_distance paths rates state
-             
-         )
-              
+      let (res, state_map) = let Cave_state.{ closed_valves; actors; _} = state in
+      let  [@warning "-8"] actor::rest_actors = List.sort actors ~compare:(fun a1 a2 -> Int.compare a2.minutes_remaining a1.minutes_remaining) in
+      let Actor_state.{ minutes_remaining; rate; total_volume; current_room; _} = actor in
+      if minutes_remaining = 0 then begin
+          assert (List.for_all actors ~f:(fun { minutes_remaining; _ } -> minutes_remaining = 0));
+          (List.sum (module Int) actors ~f:(fun {total_volume; _} -> total_volume), state_map)
+        end else if String.Set.length closed_valves = 0 then
+        (List.sum (module Int) actors ~f:(fun {total_volume; rate; minutes_remaining; _} -> total_volume + rate * minutes_remaining), state_map)
+      else
+        let nexts: (string * int) list = String.Map.find_multi paths current_room 
+                                         |> List.filter ~f:(fun (room, _) -> String.Set.mem closed_valves room) in
+        List.fold_left nexts ~init:(0, state_map) ~f:(fun (prev_total, state_map) (new_current_room, distance) ->
+               let available_distance = if distance < minutes_remaining then distance else minutes_remaining - 1 in
+               let new_rate = rate + String.Map.find_exn rates new_current_room in
+               let total_volume = total_volume + rate * (available_distance + 1) in
+               (*assert (not (String.Set.mem closed_valves current_room)); *)
+               let closed_valves = String.Set.remove closed_valves new_current_room in
+               let actor = Actor_state.{minutes_remaining = minutes_remaining - available_distance - 1; total_volume;  rate = new_rate; current_room = new_current_room  } in
+               let state = Cave_state.{closed_valves; actors = (List.sort (actor::rest_actors)  ~compare:(fun a1 a2 -> Int.compare a2.minutes_remaining a1.minutes_remaining)) } in
+               let (res, state_map) = if available_distance < distance then
+                   (List.sum (module Int) state.actors ~f:(fun {total_volume; _} -> total_volume), state_map)
+                 else
+                   walk_paths_with_distance paths rates state ~state_map in
+               if res > prev_total then (res, state_map) else (prev_total, state_map)      
+          ) in
+      (res, Cave_state_map.add_exn state_map ~key:state ~data:res)
+      
 let line_re = Re.Perl.compile_pat "Valve ([A-Z]+) has flow rate=([0-9]+); tunnel(s?) lead(s?) to valve(s?) ([A-Z ,]+)"
 let to_valves_split_re = Re.Perl.compile_pat ", "
 let parse s =
@@ -110,11 +100,9 @@ let part_1_fast s =
   let rates = List.map l ~f:(fun (v, d, _) -> (v, d)) |> String.Map.of_alist_exn in
   let paths_with_distance = shortest_paths paths rates in
   let closed_valves = String.Map.filter rates  ~f:(fun r -> r > 0) |> String.Map.keys |> String.Set.of_list in
-  let actor_1 = {idx = 1; total_volume = 0; rate = 0; minutes_remaining = 30; current_room = "AA"; } in
-  let start = { closed_valves; trace = []; actors = [actor_1]} in
-  let (res, trace) = walk_paths_with_distance paths_with_distance rates start
-                     |> List.max_elt ~compare:(fun (a, _) (b, _) -> Int.compare a b) |> Option.value_exn in
-  print_endline (String.concat ~sep:"\n\n" (List.rev trace));
+  let actor_1 = Actor_state.{total_volume = 0; rate = 0; minutes_remaining = 30; current_room = "AA"; } in
+  let start = Cave_state.{ closed_valves; actors = [actor_1]} in
+  let (res, _) = walk_paths_with_distance paths_with_distance rates start ~state_map:Cave_state_map.empty in
   Printf.printf "Res: %d\n" res
   
 let [@warning "-32"] big_input = In_channel.read_all "day16/input.txt"
@@ -127,14 +115,12 @@ let [@warning "-32"] part_2_fast s =
   let rates = List.map l ~f:(fun (v, d, _) -> (v, d)) |> String.Map.of_alist_exn in
   let paths_with_distance = shortest_paths paths rates in
   let closed_valves = String.Map.filter rates  ~f:(fun r -> r > 0) |> String.Map.keys |> String.Set.of_list in
-  let actor_1 = {idx = 1; total_volume = 0; rate = 0; minutes_remaining = 26; current_room = "AA"; } in
-  let actor_2 = {idx = 2; total_volume = 0; rate = 0; minutes_remaining = 26; current_room = "AA"; } in
-  let start = { closed_valves; trace = []; actors = [actor_1; actor_2]} in
-  let (res, trace) = walk_paths_with_distance paths_with_distance rates start
-                     |> List.max_elt ~compare:(fun (a, _) (b, _) -> Int.compare a b) |> Option.value_exn in
-  print_endline (String.concat ~sep:"\n\n" (List.rev trace));
+  let actor_1 = Actor_state.{ total_volume = 0; rate = 0; minutes_remaining = 26; current_room = "AA"; } in
+  let actor_2 = Actor_state.{ total_volume = 0; rate = 0; minutes_remaining = 26; current_room = "AA"; } in
+  let start = Cave_state.{ closed_valves; actors = [actor_1; actor_2]} in
+  let (res, _) = walk_paths_with_distance paths_with_distance rates start ~state_map:Cave_state_map.empty in
   Printf.printf "Res: %d\n" res
-  
+ 
 let [@warning "-32"] big_input = In_channel.read_all "day16/input.txt"
                 |> (String.strip ~drop:Char.(fun c -> c = '\n'))
                                
